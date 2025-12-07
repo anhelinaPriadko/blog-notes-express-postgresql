c04842dd7df5709fac4898ca673f216146426716
unknown <anhelina.pryadko@gmail.com>
2025-12-07 14:55:49 +0200

    change queries.sql with the whole database dump

--- queries.sql
+++ queries.sql
@@ -1,38 +1,434 @@
-create table books (
-id serial primary key,
-name varchar(100) not null,
-ISBN varchar(13) not null,
-sImage text,
-mInmage text,
-lImage text,
-rating integer not null check (rating >= 1 and rating <= 5)
+--
+-- PostgreSQL database dump
+--
+
+\restrict HKOg5vHF8LT9wamVhJMLVlpQqa73avrThceqRpSxHQnuImprtzlaQI3EE95Mw2a
+
+-- Dumped from database version 17.6
+-- Dumped by pg_dump version 17.6
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET transaction_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
+SET check_function_bodies = false;
+SET xmloption = content;
+SET client_min_messages = warning;
+SET row_security = off;
+
+--
+-- Name: get_books_by_filter(character varying[], character varying[]); Type: FUNCTION; Schema: public; Owner: postgres
+--
+
+CREATE FUNCTION public.get_books_by_filter(p_author_names character varying[], p_genre_names character varying[]) RETURNS TABLE(id integer, name character varying, isbn character varying, simage text)
+    LANGUAGE plpgsql
+    AS $$
+BEGIN
+    RETURN QUERY
+    SELECT DISTINCT b.id, b.name, b.isbn, b.simage, b.isdeleted
+    FROM books b
+    JOIN book_authors ba ON b.id = ba.book_id
+    JOIN authors a ON ba.author_id = a.id
+    JOIN book_genres bg ON b.id = bg.book_id
+    JOIN genres g ON bg.genre_id = g.id
+    WHERE
+        (
+            p_author_names IS NULL OR cardinality(p_author_names) = 0 
+            OR EXISTS (
+                SELECT 1 
+                FROM unnest(p_author_names) AS filter_name 
+                WHERE a.name ILIKE '%' || filter_name || '%'
+            )
+        )
+        AND
+        (
+            p_genre_names IS NULL OR cardinality(p_genre_names) = 0 
+            OR EXISTS (
+                SELECT 1 
+                FROM unnest(p_genre_names) AS filter_name 
+                WHERE g.name ILIKE '%' || filter_name || '%'
+            )
+        );
+END;
+$$;
+
+
+ALTER FUNCTION public.get_books_by_filter(p_author_names character varying[], p_genre_names character varying[]) OWNER TO postgres;
+
+--
+-- Name: update_book_review(integer, text, integer); Type: PROCEDURE; Schema: public; Owner: postgres
+--
+
+CREATE PROCEDURE public.update_book_review(IN book_review_id integer, IN newreview text, INOUT p_rows_affected integer DEFAULT 0)
+    LANGUAGE plpgsql
+    AS $$
+BEGIN
+    UPDATE book_reviews
+    SET review = newreview
+    WHERE id = book_review_id;
+    GET DIAGNOSTICS p_rows_affected = ROW_COUNT;
+END;
+$$;
+
+
+ALTER PROCEDURE public.update_book_review(IN book_review_id integer, IN newreview text, INOUT p_rows_affected integer) OWNER TO postgres;
+
+SET default_tablespace = '';
+
+SET default_table_access_method = heap;
+
+--
+-- Name: authors; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE public.authors (
+    id integer NOT NULL,
+    name character varying(100) NOT NULL
 );
 
-create table authors(
-id serial primary key,
-name varchar(100) not null
+
+ALTER TABLE public.authors OWNER TO postgres;
+
+--
+-- Name: authors_id_seq; Type: SEQUENCE; Schema: public; Owner: postgres
+--
+
+CREATE SEQUENCE public.authors_id_seq
+    AS integer
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER SEQUENCE public.authors_id_seq OWNER TO postgres;
+
+--
+-- Name: authors_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: postgres
+--
+
+ALTER SEQUENCE public.authors_id_seq OWNED BY public.authors.id;
+
+
+--
+-- Name: book_reviews; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE public.book_reviews (
+    id integer NOT NULL,
+    review text NOT NULL
 );
 
-create table books_authors(
-id serial primary key,
-book_id integer references books(id) not null,
-author_id integer references authors(id),
-unique (book_id, author_id)
+
+ALTER TABLE public.book_reviews OWNER TO postgres;
+
+--
+-- Name: books; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE public.books (
+    id integer NOT NULL,
+    name character varying(100) NOT NULL,
+    isbn character varying(13) NOT NULL,
+    simage text,
+    mimage text,
+    limage text,
+    rating integer NOT NULL,
+    isdeleted boolean NOT NULL,
+    CONSTRAINT books_rating_check CHECK (((rating >= 1) AND (rating <= 5)))
 );
 
-create table book_reviews(
-id integer primary key references books(id),
-review text not null
+
+ALTER TABLE public.books OWNER TO postgres;
+
+--
+-- Name: books_authors; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE public.books_authors (
+    id integer NOT NULL,
+    book_id integer NOT NULL,
+    author_id integer
 );
 
-create table genres(
-id serial primary key,
-name varchar(45) not null
+
+ALTER TABLE public.books_authors OWNER TO postgres;
+
+--
+-- Name: books_authors_id_seq; Type: SEQUENCE; Schema: public; Owner: postgres
+--
+
+CREATE SEQUENCE public.books_authors_id_seq
+    AS integer
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER SEQUENCE public.books_authors_id_seq OWNER TO postgres;
+
+--
+-- Name: books_authors_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: postgres
+--
+
+ALTER SEQUENCE public.books_authors_id_seq OWNED BY public.books_authors.id;
+
+
+--
+-- Name: books_genres; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE public.books_genres (
+    id integer NOT NULL,
+    book_id integer NOT NULL,
+    genre_id integer NOT NULL
 );
 
-create table books_genres(
-id serial primary key,
-book_id integer references books(id) not null,
-genre_id integer references genres(id) not null,
-unique (book_id, genre_id)
+
+ALTER TABLE public.books_genres OWNER TO postgres;
+
+--
+-- Name: books_genres_id_seq; Type: SEQUENCE; Schema: public; Owner: postgres
+--
+
+CREATE SEQUENCE public.books_genres_id_seq
+    AS integer
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER SEQUENCE public.books_genres_id_seq OWNER TO postgres;
+
+--
+-- Name: books_genres_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: postgres
+--
+
+ALTER SEQUENCE public.books_genres_id_seq OWNED BY public.books_genres.id;
+
+
+--
+-- Name: books_id_seq; Type: SEQUENCE; Schema: public; Owner: postgres
+--
+
+CREATE SEQUENCE public.books_id_seq
+    AS integer
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER SEQUENCE public.books_id_seq OWNER TO postgres;
+
+--
+-- Name: books_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: postgres
+--
+
+ALTER SEQUENCE public.books_id_seq OWNED BY public.books.id;
+
+
+--
+-- Name: genres; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE public.genres (
+    id integer NOT NULL,
+    name character varying(45) NOT NULL
 );
+
+
+ALTER TABLE public.genres OWNER TO postgres;
+
+--
+-- Name: genres_id_seq; Type: SEQUENCE; Schema: public; Owner: postgres
+--
+
+CREATE SEQUENCE public.genres_id_seq
+    AS integer
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER SEQUENCE public.genres_id_seq OWNER TO postgres;
+
+--
+-- Name: genres_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: postgres
+--
+
+ALTER SEQUENCE public.genres_id_seq OWNED BY public.genres.id;
+
+
+--
+-- Name: authors id; Type: DEFAULT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.authors ALTER COLUMN id SET DEFAULT nextval('public.authors_id_seq'::regclass);
+
+
+--
+-- Name: books id; Type: DEFAULT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.books ALTER COLUMN id SET DEFAULT nextval('public.books_id_seq'::regclass);
+
+
+--
+-- Name: books_authors id; Type: DEFAULT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.books_authors ALTER COLUMN id SET DEFAULT nextval('public.books_authors_id_seq'::regclass);
+
+
+--
+-- Name: books_genres id; Type: DEFAULT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.books_genres ALTER COLUMN id SET DEFAULT nextval('public.books_genres_id_seq'::regclass);
+
+
+--
+-- Name: genres id; Type: DEFAULT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.genres ALTER COLUMN id SET DEFAULT nextval('public.genres_id_seq'::regclass);
+
+
+--
+-- Name: authors authors_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.authors
+    ADD CONSTRAINT authors_pkey PRIMARY KEY (id);
+
+
+--
+-- Name: book_reviews book_reviews_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.book_reviews
+    ADD CONSTRAINT book_reviews_pkey PRIMARY KEY (id);
+
+
+--
+-- Name: books_authors books_authors_book_id_author_id_key; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.books_authors
+    ADD CONSTRAINT books_authors_book_id_author_id_key UNIQUE (book_id, author_id);
+
+
+--
+-- Name: books_authors books_authors_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.books_authors
+    ADD CONSTRAINT books_authors_pkey PRIMARY KEY (id);
+
+
+--
+-- Name: books_genres books_genres_book_id_genre_id_key; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.books_genres
+    ADD CONSTRAINT books_genres_book_id_genre_id_key UNIQUE (book_id, genre_id);
+
+
+--
+-- Name: books_genres books_genres_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.books_genres
+    ADD CONSTRAINT books_genres_pkey PRIMARY KEY (id);
+
+
+--
+-- Name: books books_isbn_key; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.books
+    ADD CONSTRAINT books_isbn_key UNIQUE (isbn);
+
+
+--
+-- Name: books books_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.books
+    ADD CONSTRAINT books_pkey PRIMARY KEY (id);
+
+
+--
+-- Name: genres genres_name_key; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.genres
+    ADD CONSTRAINT genres_name_key UNIQUE (name);
+
+
+--
+-- Name: genres genres_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.genres
+    ADD CONSTRAINT genres_pkey PRIMARY KEY (id);
+
+
+--
+-- Name: book_reviews book_reviews_id_fkey; Type: FK CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.book_reviews
+    ADD CONSTRAINT book_reviews_id_fkey FOREIGN KEY (id) REFERENCES public.books(id);
+
+
+--
+-- Name: books_authors books_authors_author_id_fkey; Type: FK CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.books_authors
+    ADD CONSTRAINT books_authors_author_id_fkey FOREIGN KEY (author_id) REFERENCES public.authors(id);
+
+
+--
+-- Name: books_authors books_authors_book_id_fkey; Type: FK CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.books_authors
+    ADD CONSTRAINT books_authors_book_id_fkey FOREIGN KEY (book_id) REFERENCES public.books(id);
+
+
+--
+-- Name: books_genres books_genres_book_id_fkey; Type: FK CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.books_genres
+    ADD CONSTRAINT books_genres_book_id_fkey FOREIGN KEY (book_id) REFERENCES public.books(id);
+
+
+--
+-- Name: books_genres books_genres_genre_id_fkey; Type: FK CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.books_genres
+    ADD CONSTRAINT books_genres_genre_id_fkey FOREIGN KEY (genre_id) REFERENCES public.genres(id);
+
+
+--
+-- PostgreSQL database dump complete
+--
+
+\unrestrict HKOg5vHF8LT9wamVhJMLVlpQqa73avrThceqRpSxHQnuImprtzlaQI3EE95Mw2a
+
